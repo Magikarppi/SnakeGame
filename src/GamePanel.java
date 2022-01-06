d11ffe08f75fbcca049452dba7093a12e1e0e905
Sami H <samih90@gmail.com>
2022-01-06 19:58:23 +0200

    GamePanel variables and methods, empty game screen shows

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,8 +1,80 @@
-import javax.swing.JPanel;
-import javax.swing.event.AncestorListener;
+import javax.swing.*;
+import javax.swing.Timer;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class GamePanel extends JPanel implements AncestorListener {
+public class GamePanel extends JPanel implements ActionListener {
+  
+  static final int SCREEN_WIDTH = 600;
+  static final int SCREEN_HEIGHT = 600;
+  static final int UNIT_SIZE = 25;
+  static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+  static final int DELAY = 75;
+  final int x[] = new int[GAME_UNITS];
+  final int y[] = new int[GAME_UNITS];
+  int bodyParts = 6;
+  int applesEaten;
+  int appleX;
+  int appleY;
+  char direction = 'R'; // R | L | Y | D
+  boolean running = false;
+  Timer timer;
+  Random random;
+
   GamePanel() {
+    random = new Random();
+    this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+    this.setBackground(Color.darkGray);
+    this.setFocusable(true);
+    this.addKeyListener(new MyKeyAdapter());
+    startGame();
+  }
+
+  public void startGame() {
+    newApple();
+    running = true;
+    timer = new Timer(DELAY, this);
+    timer.start();
+  }
+
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    draw(g);
+  }
+
+  public void draw(Graphics g) {
     
+  }
+
+  public void newApple() {
+    
+  }
+
+  public void move() {
+    
+  }
+  public void checkApple() {
+    
+  }
+
+  public void checkCollisions() {
+    
+  }
+
+  public void gameOver(Graphics g) {
+    
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent event) {
+    
+  }
+  public class MyKeyAdapter extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e) {
+      
+    }
   }
 }
